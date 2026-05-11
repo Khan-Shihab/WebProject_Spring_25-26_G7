6a8f5fda8892b69ad2c59f045d82b39df22f8912
Shihab Khan <22-49326-3@student.aiub.edu>
2026-05-11 11:06:20 -0500

    initialize ajax file and get value by element

---
+++ HotelRoomBookingSystem/ajax/validateRegister.js
@@ -0,0 +1,11 @@
+function validateRegister()
+{
+    let name        = document.getElementById("name").value;
+    let email       = document.getElementById("email").value;
+    let phone       = document.getElementById("phone").value;
+    let nationality = document.getElementById("nationality").value;
+    let password    = document.getElementById("password").value;
+    let confirm     = document.getElementById("confirm_password").value;
+
+    
+}
