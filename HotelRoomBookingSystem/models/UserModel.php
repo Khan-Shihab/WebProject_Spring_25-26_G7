e0a241642558a027d6ff85e37233bdaf5ef30a5b
Shihab Khan <22-49326-3@student.aiub.edu>
2026-05-11 14:30:51 -0500

    check existing email function

--- HotelRoomBookingSystem/models/UserModel.php
+++ HotelRoomBookingSystem/models/UserModel.php
@@ -13,4 +13,13 @@ function createUser($connection,$name,$email,$password,$phone,$nationality){
     }
 }
 
+function emailExists($connection, $email) {
+    $sql = "SELECT id FROM users WHERE email = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result->num_rows > 0;
+}
+
 >?
