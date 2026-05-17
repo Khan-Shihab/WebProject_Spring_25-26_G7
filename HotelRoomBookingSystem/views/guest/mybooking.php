6b2d1786b5ce21e34f4bad72ba67b463bf010e31
MD Fahim Montasim RaFe <mdfahimmontasimrafe@gmail.com>
2026-05-17 16:30:17 +0600

    table head

---
+++ HotelRoomBookingSystem/views/guest/mybooking.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title> All my Booking Rooms  </title>
+    </head>
+
+    <body>
+    <h2>My Bookings</h2>
+    <p>All my reservations</p>
+
+    <table>
+        <tr>
+            <th>id</th>
+            <th>Room Type</th>
+            <th>Check-in</th>
+            <th>Check-out</th>
+            <th>Total</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+
+
+
+    </table>
+    </body>
+</html>
