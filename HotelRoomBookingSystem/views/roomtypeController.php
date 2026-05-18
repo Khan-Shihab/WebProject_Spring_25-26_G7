a3356311b4de84560c7d7e553d9295c1d2a9cbbc
shakib0909 <shakib090457@gmail.com>
2026-05-18 11:36:29 +0600

    Refactor roomtypeController.php for room type addition

--- HotelRoomBookingSystem/views/roomtypeController.php
+++ HotelRoomBookingSystem/views/roomtypeController.php
@@ -8,3 +8,19 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: ../views/auth/login.php");
     exit();
 }
+$database = new db();
+$connection = $database->connection();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_room_type'])) {
+
+    $name = trim($_POST['name']);
+    $description = trim($_POST['description']);
+    $price_per_night = $_POST['price_per_night'];
+    $max_capacity = $_POST['max_capacity'];
+
+    $amenities = "";
+    if (isset($_POST['amenities'])) {
+        $amenities = implode(",", $_POST['amenities']);
+    }
+
+
